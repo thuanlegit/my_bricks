0b75219603fe1aa9814a7b0883100d9fbda8ff38
lokishengoku <dthuanle.20@gmail.com>
2022-09-06 17:08:54 +0700

    add name & route to page

--- bloc_view/__brick__/{{view_name.snakeCase()}}_page.dart
+++ bloc_view/__brick__/{{view_name.snakeCase()}}_page.dart
@@ -5,6 +5,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '{{view_name.snakeCase()}}_view.dart';
 
 class {{view_name.pascalCase()}}Page extends StatelessWidget {
+  static const String name = '/{{view_name.snakeCase()}}';
+
+  static Route<void> route() {
+    return MaterialPageRoute(
+      builder: (ctx) {
+        return BlocProvider(
+          create: (context) => {{view_name.pascalCase()}}Bloc(),
+          child: const {{view_name.pascalCase()}}View(),
+        );
+      },
+    );
+  }
+
   const {{view_name.pascalCase()}}Page({
     Key? key,
   }) : super(key: key);
