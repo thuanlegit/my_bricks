17891b2e44ea846bc9749fd0be0a57273da446ad
lokishengoku <dthuanle.20@gmail.com>
2022-09-06 17:10:39 +0700

    fix missing enum

--- freezed_bloc/__brick__/{{bloc_name.snakeCase()}}_state.dart
+++ freezed_bloc/__brick__/{{bloc_name.snakeCase()}}_state.dart
@@ -1,5 +1,7 @@
 part of '{{bloc_name.snakeCase()}}_bloc.dart';
 
+enum Load{{bloc_name.pascalCase()}}State { initial, loading, loaded, error }
+
 @freezed
 class {{bloc_name.pascalCase()}}State with _${{bloc_name.pascalCase()}}State {
   const factory {{bloc_name.pascalCase()}}State({
