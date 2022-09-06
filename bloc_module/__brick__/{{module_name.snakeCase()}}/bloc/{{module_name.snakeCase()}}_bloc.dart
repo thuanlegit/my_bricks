9ead795e41c8234c981ed1b4adda44f5bb858919
lokishengoku <dthuanle.20@gmail.com>
2022-09-06 15:50:26 +0700

    remove output option

--- bloc_module/__brick__/{{module_name.snakeCase()}}/bloc/{{module_name.snakeCase()}}_bloc.dart
+++ bloc_module/__brick__/{{module_name.snakeCase()}}/bloc/{{module_name.snakeCase()}}_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-part '{{module_name}}_event.dart';
-part '{{module_name}}_state.dart';
+part '../../{{% output %}}/{{module_name.snakeCase()}}/bloc/{{module_name}}_event.dart';
+part '../../{{% output %}}/{{module_name.snakeCase()}}/bloc/{{module_name}}_state.dart';
 
 class {{module_name.pascalCase()}}Bloc extends Bloc<{{module_name.pascalCase()}}Event, {{module_name.pascalCase()}}State> {
   
